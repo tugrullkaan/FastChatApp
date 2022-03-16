eb345046e6d195cca7c0bb540ba2770a27f69f07
mehmet tuğrul kaan <61920968+tugrullkaan@users.noreply.github.com>
2022-03-16 14:27:37 +0300

    FastChatApp
    FastChatApp_Android_Project

---
+++ app/src/main/java/com/mtugrulkaan/fastchatapp/MainActivity.java
@@ -0,0 +1,31 @@
+package com.mtugrulkaan.fastchatapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+
+
+            @Override
+            public void run() {
+                // Bu method zamanlayıcı bittiğinde çalışacak
+                Intent i = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 5000);
+    }
+}
---
+++ app/src/main/java/com/mtugrulkaan/fastchatapp/MessagesAdater.java
@@ -0,0 +1,110 @@
+package com.mtugrulkaan.fastchatapp;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+import static com.mtugrulkaan.fastchatapp.ChatActivity.rImage;
+import static com.mtugrulkaan.fastchatapp.ChatActivity.sImage;
+
+public class MessagesAdater extends RecyclerView.Adapter {
+
+    Context context;
+    ArrayList<Messages> messagesArrayList;
+    int ITEM_SEND = 1;
+    int ITEM_RECIVE = 2;
+
+    public MessagesAdater(Context context, ArrayList<Messages> messagesArrayList) {
+        this.context = context;
+        this.messagesArrayList = messagesArrayList;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        if (viewType == ITEM_SEND) {
+            View view = LayoutInflater.from(context).inflate(R.layout.sender_layout_item, parent, false);
+            return new SenderViewHolder(view);
+        } else {
+            View view = LayoutInflater.from(context).inflate(R.layout.reciver_layout_item, parent, false);
+            return new ReciverViewHolder(view);
+        }
+
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+        Messages messages = messagesArrayList.get(position);
+
+        if (holder.getClass() == SenderViewHolder.class) {
+            SenderViewHolder viewHolder = (SenderViewHolder) holder;
+            viewHolder.txtmessage.setText(messages.getMessage());
+            Picasso.get().load(sImage).into(viewHolder.circleImageView);
+
+        } else {
+            ReciverViewHolder viewHolder = (ReciverViewHolder) holder;
+            viewHolder.txtmessage.setText(messages.getMessage());
+            Picasso.get().load(rImage).into(viewHolder.circleImageView);
+        }
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return messagesArrayList.size();
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        Messages messages = messagesArrayList.get(position);
+        if (Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).getUid().equals(messages.getSenderId())) {
+            return ITEM_SEND;
+        } else {
+            return ITEM_RECIVE;
+        }
+    }
+
+    static class SenderViewHolder extends RecyclerView.ViewHolder {
+
+        CircleImageView circleImageView;
+        TextView txtmessage;
+
+        public SenderViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            circleImageView = itemView.findViewById(R.id.profile_image);
+            txtmessage = itemView.findViewById(R.id.txtMessages);
+
+        }
+    }
+
+    static class ReciverViewHolder extends RecyclerView.ViewHolder {
+
+        CircleImageView circleImageView;
+        TextView txtmessage;
+
+        public ReciverViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            circleImageView = itemView.findViewById(R.id.profile_image);
+            txtmessage = itemView.findViewById(R.id.txtMessages);
+
+        }
+    }
+}
